fc81edbb75e599fdac69741d7370cd82cd97f78a
hwang <hgh1057@gmail.com>
2023-12-14 23:31:36 +0900

    hotfix : CORS오류 rewrite설정

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API}/:path*`,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
--- src/api/api-instance.ts
+++ src/api/api-instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API,
+  baseURL: '/',
   headers: {
     'Content-Type': 'application/json',
   },
