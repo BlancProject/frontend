dd4b3d778dd68d71a8dbce47c42f01932fa4ffc2
hwang <hgh1057@gmail.com>
2023-12-23 13:36:39 +0900

    feat: 로그인 안 했을시 예외처리

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,8 +1,22 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+
 import CartBox from '@/components/Cart/Box/CartBox';
 import Footer from '@/components/Cart/Footer';
 import CartSubTitle from '@/components/Cart/SubTitle/CartSubTitle';
+import useToken from '@/hooks/useAuthCheck';
 
 export default function CartPage() {
+  const router = useRouter();
+  const hasToken = useToken();
+  useEffect(() => {
+    if (hasToken) {
+      alert('로그인 먼저 해주세요!');
+      router.push('/login');
+    }
+  }, [hasToken]);
   return (
     <div className="all flex flex-col h-screen">
       <div className="body mx-40 pb-32">
