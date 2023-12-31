dcd4f9859f7fe99bf02890ca2440afea559ce888
hwang <hgh1057@gmail.com>
2023-12-31 21:58:05 +0900

    배포Test

--- src/api/api-instance.ts
+++ src/api/api-instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const baseInstance = axios.create({
-  baseURL: '/',
+  baseURL: process.env.NEXT_PUBLIC_API,
   headers: {
     'Content-Type': 'application/json',
   },
